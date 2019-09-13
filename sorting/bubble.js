cb37732e72f1e5f88289ead2a907532104c33728
khoi <khoiracle@gmail.com>
2019-09-13 17:04:50 +0700

    Add a break when no swapped made

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -4,14 +4,21 @@ class BubbleSort {
   }
 
   *makeGenerator() {
-    for (let i = 0; i < this.values.length; i++) {
-      for (let j = 0; j < this.values.length - i - 1; j++) {
+    let swapped = false;
+    let i, j;
+    for (i = 0; i < this.values.length; i++) {
+      for (j = 0; j < this.values.length - i - 1; j++) {
         if (values[j] > values[j + 1]) {
           let t = values[j];
           values[j] = values[j + 1];
           values[j + 1] = t;
+          swapped = true;
         }
         yield j + 1;
+      }
+
+      if (!swapped) {
+        return;
       }
     }
   }
