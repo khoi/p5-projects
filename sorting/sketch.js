8b81d62a6e4faf42f42399e1dfbd43dafcaca017
khoi <khoiracle@gmail.com>
2019-09-14 15:24:33 +0700

    Full screen

--- sorting/sketch.js
+++ sorting/sketch.js
@@ -1,28 +1,34 @@
-let values;
-let N = 50;
-let columnWidth;
 const FRAME_RATE = 60;
+const CONTROL_HEIGHT = 20;
+
+let N = 50;
+let M = 10;
+
+let values;
+let tileWidth;
+let tileHeight;
 let sorters;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
   colorMode(HSL, 360, 100, 100);
 
   frameRate(FRAME_RATE);
 
-  values = new Array(N);
-  sorters = new Array(N);
+  tileWidth = width / N;
+  M = Math.floor(windowHeight / tileWidth);
 
-  for (let i = 0; i < N; i++) {
+  values = new Array(M);
+  sorters = new Array(M);
+
+  for (let i = 0; i < M; i++) {
     values[i] = new Array(N);
     for (let j = 0; j < N; j++) {
       values[i][j] = j;
     }
     shuffle(values[i], true);
-    sorters[i] = new BubbleSort(values[i]).makeGenerator();
+    sorters[i] = new InsertionSort(values[i]).makeGenerator();
   }
-
-  columnWidth = width / N;
 
   textSize(20);
   textStyle(BOLD);
@@ -30,13 +36,12 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  for (let i = 0; i < N; i++) {
+  for (let i = 0; i < M; i++) {
     for (let j = 0; j < N; j++) {
       let c = color(map(values[i][j], 0, N, 0, 360), 100, 50);
       stroke(c);
       fill(c);
-      rect(j * columnWidth, i * columnWidth, columnWidth, columnWidth);
+      rect(j * tileWidth, i * tileWidth, tileWidth, tileWidth);
     }
     sorters[i].next();
   }
