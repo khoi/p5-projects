7697276342675ea8a8cde58d3f9b5bbe3d4f0a8c
khoi <khoiracle@gmail.com>
2019-09-14 15:43:47 +0700

    Add ability to control algorithms

--- sorting/sketch.js
+++ sorting/sketch.js
@@ -9,11 +9,17 @@ let tileWidth;
 let tileHeight;
 let sorters;
 
+const SORT_CLASSES = {
+  bubble: BubbleSort,
+  insertion: InsertionSort
+};
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSL, 360, 100, 100);
-
   frameRate(FRAME_RATE);
+
+  setupInput();
 
   tileWidth = width / N;
   M = Math.floor(windowHeight / tileWidth);
@@ -35,6 +41,28 @@ function setup() {
   textAlign(RIGHT);
 }
 
+function setupInput() {
+  Object.keys(SORT_CLASSES).forEach((k, i) => {
+    let button = createButton(k);
+    button.position(60 * i + 20, 20);
+    button.mousePressed(() => startSorting(k));
+  });
+}
+
+function startSorting(algorithm) {
+  values = new Array(M);
+  sorters = new Array(M);
+
+  for (let i = 0; i < M; i++) {
+    values[i] = new Array(N);
+    for (let j = 0; j < N; j++) {
+      values[i][j] = j;
+    }
+    shuffle(values[i], true);
+    sorters[i] = new SORT_CLASSES[algorithm](values[i]).makeGenerator();
+  }
+}
+
 function draw() {
   for (let i = 0; i < M; i++) {
     for (let j = 0; j < N; j++) {
@@ -45,6 +73,10 @@ function draw() {
     }
     sorters[i].next();
   }
+  drawFrameRate();
+}
+
+function drawFrameRate() {
   fill(255, 0, 0);
   text(Math.floor(frameRate()), width - 30, 30);
 }
